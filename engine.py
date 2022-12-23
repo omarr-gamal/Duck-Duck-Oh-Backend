c1fb793b0bb9bb02397d952f72cc5bc839330bb4
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2022-12-23 23:32:10 +0200

    added searching and ranking to engine

--- engine.py
+++ engine.py
@@ -21,12 +21,20 @@ class Engine:
         
         index_dict = json.loads(index.index)
         return index_dict
+    
+    def __update_index(self, index_dict):
+        index_json = json.dumps(index_dict)
+        
+        index = Index.query.first()
+        index.index = index_json
+        
+        index.update()
 
     def add_document(self, body):
-        document = Document(body)
+        document = Document(body=body)
         document.insert()
         
-        self.__index_document(Document)
+        self.__index_document(document)
 
     def __index_document(self, document):
         nltk.download('punkt')
@@ -45,23 +53,15 @@ class Engine:
         
         self.__update_index(index)
 
-    def __update_index(self, index_dict):
-        index_json = json.dumps(index_dict)
-        
-        index = Index.query.first()
-        index.index = index_json
-        
-        index.update()
-
     # Extract text and image tags from an HTML document
-    def __extract_text_and_tags(html_doc):
+    def __extract_text_and_tags(self, html_doc):
         soup = BeautifulSoup(html_doc, 'html.parser')
         text = soup.get_text()
         image_tags = soup.find_all('img')
         return text, image_tags
 
     # Tokenize the text and image tags
-    def __tokenize(text, image_tags):
+    def __tokenize(self, text, image_tags):
         text_tokens = word_tokenize(text)
         image_tokens = []
         for tag in image_tags:
@@ -72,34 +72,45 @@ class Engine:
             if alt:
                 image_tokens.extend(re.findall(r'\b\w+\b', alt))
         return text_tokens, image_tokens
-
-    # Search the index for a given search term
-    def search(self, search_term, documents):
-        index = self.__get_index()
-        
+    
+    def search(self, search_term):
+        # Tokenize the search term
         search_tokens = word_tokenize(search_term)
-        
-        matching_doc_ids = []
+        # Initialize a set to keep track of the documents that contain all the tokens in the search term
+        matching_documents = set()
+        # Iterate over the tokens in the search term
         for token in search_tokens:
-            if token in index:
-                matching_doc_ids.extend(index[token])
-        matching_doc_ids = list(set(matching_doc_ids))
-        
+            # Get the documents that contain the current token
+            documents = self.__get_index().get(token, [])
+            # If this is the first token, initialize the matching_documents set with the documents that contain the token
+            if not matching_documents:
+                matching_documents = set(documents)
+            # Otherwise, update the matching_documents set to only include documents that contain all the tokens in the search term
+            else:
+                matching_documents &= set(documents)
         # Rank the documents based on how closely they match the search term
-        ranked_docs = []
-        for doc_id in matching_doc_ids:
-            doc = documents[doc_id]
-            similarity = difflib.SequenceMatcher(None, search_term, doc).ratio()
-            ranked_docs.append((doc_id, similarity))
-        ranked_docs.sort(key=lambda x: x[1], reverse=True)
-        return [documents[doc_id] for doc_id, _ in ranked_docs]
-
-# Test the search engine
-# documents = [
-#     '<html><body>This is a test document</body></html>',
-#     '<html><body>This is another test document</body></html>',
-#     '<html><body>This is a third test document</body></html>',
-# ]
-# index = create_index(documents)
-# search_results = search(index, 'test document', documents)
-# print(search_results)  # Should print the three test documents
+        ranked_documents = sorted(matching_documents, key=lambda doc_id: self.__rank_document(doc_id, search_term))
+        # Return the ranked document ids
+        return ranked_documents
+    
+    def __rank_document(self, doc_id, search_term):
+        # Fetch the document with the given id
+        document = Document.query.get(doc_id)
+        # Tokenize the document
+        nltk.download('punkt')
+        text, image_tags = self.__extract_text_and_tags(document.body)
+        text_tokens, image_tokens = self.__tokenize(text, image_tags)
+        tokens = text_tokens + image_tokens
+        # Tokenize the search term
+        search_tokens = word_tokenize(search_term)
+        # Initialize a count to keep track of the number of occurrences of the search term in the document
+        count = 0
+        # Iterate over the tokens in the document
+        for i, token in enumerate(tokens):
+            # Check if the current token is the first token in the search term
+            if token == search_tokens[0]:
+                # Check if the remaining tokens in the document match the search term
+                if tokens[i:i+len(search_tokens)] == search_tokens:
+                    count += 1
+        # Return the count as the rank
+        return count
