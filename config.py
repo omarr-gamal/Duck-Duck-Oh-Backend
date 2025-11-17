dbfbf5ebdb77362091659c020646a0f25b080b86
Omar Gamal <omogamal2002@gmail.com>
2025-11-17 13:50:27 +0200

    update database environment variables

--- config.py
+++ config.py
@@ -11,13 +11,16 @@ DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = ''
 
-if os.environ['env'] == 'prod':
+if os.environ.get('ENV', "dev") == 'prod':
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres', 'postgresql')
 else:
-    db_user = os.environ['DB_USER']
-    db_pass = os.environ['DB_PASS']
+    db_user = os.environ.get('DB_USER', "postgres")
+    db_pass = os.environ.get('DB_PASS', "postgres")
+    db_name = os.environ.get('DB_NAME', "duck_duck_oh")
+    db_base_url = os.environ.get('DB_BASE_URL', "localhost:5432")
 
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{db_user}:{db_pass}@localhost:5432/duck_duck_oh'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{db_user}:{db_pass}@{db_base_url}/{db_name}'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
